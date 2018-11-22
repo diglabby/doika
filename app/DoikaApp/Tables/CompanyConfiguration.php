9548f798df8f1993a0b1c674c5cfed09d6ee6d76
Tyuba4 <pojarkov@tut.by>
2018-11-22 14:32:52 +0300

    Add main tables and model for work with them

---
+++ app/DoikaApp/Tables/CompanyConfiguration.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\DoikaApp\Tables;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CompanyConfiguration extends Model
+{
+    //связи модели один-ко-многим в которых она является дочерней
+    public function company(){
+        return $this->belongsTo('App\DoikaApp\Tables\Company');
+    }
+}
---
+++ database/migrations/2018_11_22_060912_create_payments_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePaymentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('company_id');
+            $table->foreign('company_id')->references('id')->on('companies');
+            $table->double('amount');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payments');
+    }
+}
