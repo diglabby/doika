63add4c18c8da64969d9402f3f28f724eca4f6de
Alex lptn <web.lapateen@gmail.com>
2019-01-24 01:58:06 +0300

    #178 Выкарыстана now() функцыя замест захардкоджаных дат у UsersSeeder

--- database/seeds/UsersSeeder.php
+++ database/seeds/UsersSeeder.php
@@ -20,16 +20,16 @@ class UsersSeeder extends Seeder
         return factory(User::class)->create([
             'name' => 'Demo',
             'email' => 'demo@example.com',
-            'email_verified_at' => '2018-10-24 16:13:17',
+            'email_verified_at' => now(),
             'password' => '$2y$10$AQlsiM6XRfGZXRdu58Qo7OjVRRkZCXNekV/lst2OQUKCX59FDH/Ie',
             'active' => 1,
             'remember_token' => 'P6oRtEtT9rLLCcd1YHudQMyFYUsfysWfOcDE7pPdye2rCZbvKbF3PnaUbgRP',
             'locale' => 'en',
             'timezone' => 'UTC',
             'slug' => 'demo',
-            'last_access_at' => '2018-11-07 19:02:108',
-            'created_at' => '2018-10-24 16:13:17',
-            'updated_at' => '2018-11-07 19:02:108',
+            'last_access_at' => null,
+            'created_at' => now(),
+            'updated_at' => now(),
         ]);
     }
 }
