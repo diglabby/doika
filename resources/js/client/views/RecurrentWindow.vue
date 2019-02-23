6c808ae7ac40fbdd0138c6538664203ee7efbc3b
ashkat <ashkatcrowdog@gmail.com>
2019-02-23 14:54:41 +0300

    Text fix

--- resources/js/client/views/RecurrentWindow.vue
+++ resources/js/client/views/RecurrentWindow.vue
@@ -34,6 +34,32 @@ export default {
       types: ['Name', 'Mail', 'Phone']
     }
   },
+  // start decision
+  methods: {
+    type: function (e) {
+      this.errors = [];
+
+      if (!this.name) {
+        this.errors.push('Укажите имя.');
+      }
+      if (!this.email) {
+        this.errors.push('Укажите электронную почту.');
+      } else if (!this.validEmail(this.email)) {
+        this.errors.push('Укажите корректный адрес электронной почты.');
+      }
+
+      if (!this.errors.length) {
+        return true;
+      }
+      e.preventDefault();
+    },
+    validEmail: function (email) {
+      var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      return re.test(email);
+    }
+  },
+  //  end decision
+
   
   async created() {
     let { data } = await axios.get(
