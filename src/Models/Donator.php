d8cbbdb7e98fe29f8b13ee341d738ad1fa337875
Alies Lapatsin <web.lapateen@gmail.com>
2019-03-10 19:31:32 +0300

    #271 Add missing PHPDoc

--- src/Models/Donator.php
+++ src/Models/Donator.php
@@ -5,12 +5,15 @@ namespace Diglabby\Doika\Models;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Support\Carbon;
 
 /**
  * @property int $id
  * @property string $email
  * @property string|null $name
  * @property int|null $phone
+ * @property Carbon $created_at
+ * @property Carbon $updated_at
  *
  * Relationships:
  * @property-read Collection|Subscription[] $subscriptions
