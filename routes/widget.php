939f55665a1e500fb8d16ac651cb973db99bf0c8
Alies Lapatsin <web.lapateen@gmail.com>
2019-03-12 23:38:03 +0300

    #271 Remove extra route

--- routes/widget.php
+++ routes/widget.php
@@ -18,7 +18,7 @@ Route::post('campaign/{campaignId}/donate', 'Widget\CampaignController@donate')-
 Route::get('campaign/{campaignId}/recurrent/create-donator', 'Widget\CampaignController@createDonator')->name('campaign.recurrent')->where('campaignId', '[0-9]+');
 Route::post('campaign/{campaignId}/recurrent/donate', 'Widget\CampaignController@donateRecurrent')->name('campaign.recurrent.donate')->where('campaignId', '[0-9]+');
 
+/** @todo @fr0zen will remove it */
 Route::get('about', function () {})->name('about');
 Route::get('contact', function () {})->name('contact');
-Route::get('home', function () {})->name('home');
 Route::get('legal-mentions', function () {})->name('legal-mentions');
