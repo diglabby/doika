d25deb87345b0c0892d5cf1dc8f972dcdf07db21
Alies Lapatsin <web.lapateen@gmail.com>
2019-03-12 11:36:54 +0300

    #271 Remove roles-related routes
    We don't have roles for admins anymore

--- routes/admin.php
+++ routes/admin.php
@@ -5,25 +5,6 @@ Route::get('routes/search', 'AjaxController@routesSearch')->name('routes.search'
 Route::post('images/upload', 'AjaxController@imageUpload')->name('images.upload');
 
 Route::group(
-    ['middleware' => ['can:view users']],
-    function () {
-        Route::get('users/active_counter', 'UserController@getActiveUserCounter')->name('users.active.counter');
-
-        Route::get('users/roles', 'UserController@getRoles')->name('users.get_roles');
-
-        Route::get('users/search', 'UserController@search')->name('users.search');
-        Route::get('users/{user}/show', 'UserController@show')->name('users.show');
-
-        Route::resource('users', 'UserController', [
-            'only' => ['store', 'update', 'destroy'],
-        ]);
-
-        Route::post('users/batch_action', 'UserController@batchAction')->name('users.batch_action');
-        Route::post('users/{user}/active', 'UserController@activeToggle')->name('users.active');        
-    }
-);
-
-Route::group(
     ['middleware' => ['can:view campaigns']],
     function () {
         Route::get('campaigns/search', 'CampaignController@search')->name('campaigns.search');
@@ -52,21 +33,6 @@ Route::group(
         Route::post('payments/batch_action', 'PaymentController@batchAction')->name('payments.batch_action');
     }
 );
-
-Route::group(
-    ['middleware' => ['can:view roles']],
-    function () {
-        Route::get('roles/permissions', 'RoleController@getPermissions')->name('roles.get_permissions');
-
-        Route::get('roles/search', 'RoleController@search')->name('roles.search');
-        Route::get('roles/{role}/show', 'RoleController@show')->name('roles.show');
-
-        Route::resource('roles', 'RoleController', [
-            'only' => ['store', 'update', 'destroy'],
-        ]);
-    }
-);
-
 
 Route::get('/{vue_capture?}', 'DashboardController@index')
     ->where('vue_capture', '[\/\w\.-]*')
