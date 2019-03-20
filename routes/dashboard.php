306b24566ddc8abdbf7df56d0f531210d7c7bf08
Alies Lapatsin <web.lapateen@gmail.com>
2019-03-21 01:36:13 +0300

    #216 Add missing dashboard route stubs

--- routes/dashboard.php
+++ routes/dashboard.php
@@ -3,17 +3,18 @@
 
 Route::get('/campaigns', 'Dashboard\CampaignController@index')->name('dashboad.campaigns.index');
 Route::get('/campaigns/{id}', 'Dashboard\CampaignController@show')->name('dashboad.campaigns.index');
-Route::post('/campaigns', 'Dashboard\CampaignController@store')->name('dashboad.campaigns.delete');
+Route::post('/campaigns', 'Dashboard\CampaignController@store')->name('dashboad.campaigns.store');
 Route::put('/campaigns/{id}', 'Dashboard\CampaignController@update')->name('dashboad.campaigns.update');
 Route::delete('/campaigns/{id}', 'Dashboard\CampaignController@delete')->name('dashboad.campaigns.delete');
-Route::get('/campaigns/search', 'Dashboard\CampaignController@search')->name('dashboad.campaigns.search');
 
 Route::get('/transactions', 'Dashboard\TransactionController@index')->name('dashboad.transactions.index');
 Route::get('/transactions/{id}', 'Dashboard\TransactionController@show')->name('dashboad.transactions.show');
-Route::get('/transactions/search', 'Dashboard\TransactionController@search')->name('dashboad.transactions.search');
+Route::post('/transactions', 'Dashboard\TransactionController@store')->name('dashboad.transactions.store');
 
 Route::get('/donators', 'Dashboard\DonatorController@index')->name('dashboad.donators.index');
 Route::get('/donators/{id}', 'Dashboard\DonatorController@show')->name('dashboad.donators.show');
+Route::post('/donators', 'Dashboard\DonatorController@store')->name('dashboad.donators.store');
+Route::put('/donators/{id}', 'Dashboard\DonatorController@update')->name('dashboad.donators.update');
 Route::delete('/donators/{id}', 'Dashboard\DonatorController@delete')->name('dashboad.donators.delete');
 
 Route::get('/{vue_capture?}', 'Dashboard\DashboardController@index')
--- src/Http/Controllers/Dashboard/CampaignController.php
+++ src/Http/Controllers/Dashboard/CampaignController.php
@@ -4,6 +4,7 @@ namespace Diglabby\Doika\Http\Controllers\Dashboard;
 
 use App\Http\Controllers\Controller;
 use Diglabby\Doika\Models\Campaign;
+use Illuminate\Http\Request;
 
 final class CampaignController extends Controller
 {
@@ -17,6 +18,19 @@ final class CampaignController extends Controller
         return view('backend.admin.campaign', [
             'camplaign' => factory(Campaign::class)->make(['id' => $campaignId])
         ]);
+    }
+
+    public function store(Request $request)
+    {
+        $campaign = new Campaign($request->all());
+        $campaign->save();
+
+        return $campaign;
+    }
+
+    public function update(int $campaignId, Request $request)
+    {
+        return Campaign::query()->findOrFail($campaignId);
     }
 
     public function delete(Campaign $campaign)
