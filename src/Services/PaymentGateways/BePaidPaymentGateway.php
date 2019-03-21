eaecce247bb97e463e3f6b96a66b35234998d7ff
Alies Lapatsin <web.lapateen@gmail.com>
2019-03-21 12:15:09 +0300

    #312 Add a new route to display donation result page

--- src/Services/PaymentGateways/BePaidPaymentGateway.php
+++ src/Services/PaymentGateways/BePaidPaymentGateway.php
@@ -46,10 +46,9 @@ final class BePaidPaymentGateway implements OffsitePaymentGateway
                 'version' => self::API_VERSION,
                 'attempts' => 3,
                 'settings' => [
-                    'success_url' => $appUrl.'status=success',
-                    'decline_url' => $appUrl.'status=decline',
-                    'fail_url' => $appUrl.'status=fail',
-                    'cancel_url' => $appUrl.'status=cancel',
+                    'success_url' => route('widget.campaign.donation.result', ['campaignId' => $campaign->id, 'status' => 'success']),
+                    'decline_url' => route('widget.campaign.donation.result', ['campaignId' => $campaign->id, 'status' => 'decline']),
+                    'fail_url' => route('widget.campaign.donated.result', ['campaignId' => $campaign->id, 'status' => 'fail']),
                     'notification_url' => route('webhooks.bepaid.donated', [$campaign->id]),
                     'language' => $this->config->get('app.locale'),
                 ],
