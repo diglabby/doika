c3ed5f7ab7eca8cbab352d90a100cf4c14cc9fc4
Alies Lapatsin <web.lapateen@gmail.com>
2019-03-22 23:31:40 +0300

    #271 Fix typo in route name

--- routes/dashboard.php
+++ routes/dashboard.php
@@ -1,21 +1,21 @@
 <?php
 // Prefixed by /doika/dashboard
 
-Route::get('/campaigns', 'Dashboard\CampaignController@index')->name('dashboad.campaigns.index');
-Route::get('/campaigns/{id}', 'Dashboard\CampaignController@show')->name('dashboad.campaigns.index');
-Route::post('/campaigns', 'Dashboard\CampaignController@store')->name('dashboad.campaigns.store');
-Route::put('/campaigns/{id}', 'Dashboard\CampaignController@update')->name('dashboad.campaigns.update');
-Route::delete('/campaigns/{id}', 'Dashboard\CampaignController@delete')->name('dashboad.campaigns.delete');
+Route::get('/campaigns', 'Dashboard\CampaignController@index')->name('dashboard.campaigns.index');
+Route::get('/campaigns/{id}', 'Dashboard\CampaignController@show')->name('dashboard.campaigns.index');
+Route::post('/campaigns', 'Dashboard\CampaignController@store')->name('dashboard.campaigns.store');
+Route::put('/campaigns/{id}', 'Dashboard\CampaignController@update')->name('dashboard.campaigns.update');
+Route::delete('/campaigns/{id}', 'Dashboard\CampaignController@delete')->name('dashboard.campaigns.delete');
 
-Route::get('/transactions', 'Dashboard\TransactionController@index')->name('dashboad.transactions.index');
-Route::get('/transactions/{id}', 'Dashboard\TransactionController@show')->name('dashboad.transactions.show');
-Route::post('/transactions', 'Dashboard\TransactionController@store')->name('dashboad.transactions.store');
+Route::get('/transactions', 'Dashboard\TransactionController@index')->name('dashboard.transactions.index');
+Route::get('/transactions/{id}', 'Dashboard\TransactionController@show')->name('dashboard.transactions.show');
+Route::post('/transactions', 'Dashboard\TransactionController@store')->name('dashboard.transactions.store');
 
-Route::get('/donators', 'Dashboard\DonatorController@index')->name('dashboad.donators.index');
-Route::get('/donators/{id}', 'Dashboard\DonatorController@show')->name('dashboad.donators.show');
-Route::post('/donators', 'Dashboard\DonatorController@store')->name('dashboad.donators.store');
-Route::put('/donators/{id}', 'Dashboard\DonatorController@update')->name('dashboad.donators.update');
-Route::delete('/donators/{id}', 'Dashboard\DonatorController@delete')->name('dashboad.donators.delete');
+Route::get('/donators', 'Dashboard\DonatorController@index')->name('dashboard.donators.index');
+Route::get('/donators/{id}', 'Dashboard\DonatorController@show')->name('dashboard.donators.show');
+Route::post('/donators', 'Dashboard\DonatorController@store')->name('dashboard.donators.store');
+Route::put('/donators/{id}', 'Dashboard\DonatorController@update')->name('dashboard.donators.update');
+Route::delete('/donators/{id}', 'Dashboard\DonatorController@delete')->name('dashboard.donators.delete');
 
 Route::get('/{vue_capture?}', 'Dashboard\DashboardController@index')
     ->where('vue_capture', '[\/\w\.-]*')
--- routes/widget.php
+++ routes/widget.php
@@ -17,5 +17,6 @@ Route::get('campaigns/{campaignId}', 'Widget\CampaignController@show')->name('wi
 Route::get('campaigns/{campaign}/donation-result', 'Widget\CampaignDonationResultController@show')->name('widget.campaign.donation-result');
 
 Route::get('help', 'Widget\HelpController@show')->name('widget.help.show');
+
 Route::get('feedback', 'Widget\FeedbackController@show')->name('widget.feedbacks.show');
 Route::post('feedback', 'Widget\FeedbackController@store')->name('widget.feedbacks.store');
