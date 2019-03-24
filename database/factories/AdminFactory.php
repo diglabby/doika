2f76bf8f995e7ab38a8fb806d3eef0da7a01f02a
Alies Lapatsin <web.lapateen@gmail.com>
2019-03-24 20:40:18 +0300

    #288 Improve factories PHPDoc blocks

--- database/factories/AdminFactory.php
+++ database/factories/AdminFactory.php
@@ -4,16 +4,10 @@ use App\Admin;
 use Faker\Generator as Faker;
 use Illuminate\Support\Str;
 
-/*
-|--------------------------------------------------------------------------
-| Model Factories
-|--------------------------------------------------------------------------
-|
-| This directory should contain each of the model factory definitions for
-| your application. Factories provide a convenient way to generate new
-| model instances for testing / seeding your application's database.
-|
-*/
+/**
+ * @see https://github.com/fzaninotto/Faker#formatters
+ * @var \Illuminate\Database\Eloquent\Factory $factory
+ */
 
 $factory->define(Admin::class, function (Faker $faker) {
     return [
