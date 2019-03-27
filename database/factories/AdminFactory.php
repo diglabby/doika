c9b0a3c5d85dfa49a66bddcfdbff743d46c140eb
Alies Lapatsin <web.lapateen@gmail.com>
2019-03-28 01:05:03 +0300

    #325 Add tests for auth functionality

--- database/factories/AdminFactory.php
+++ database/factories/AdminFactory.php
@@ -14,6 +14,7 @@ $factory->define(Admin::class, function (Faker $faker) {
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
+        'slug' => $faker->unique()->slug,
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
     ];
---
+++ tests/Feature/Http/Controllers/Auth/LoginControllerTest.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Tests\Feature\Http\Controllers\Auth;
+
+use App\Admin;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class LoginControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    function visitor_can_open_login_page()
+    {
+        $response = $this->get(route('login'));
+
+        $response->assertOk();
+        $response->assertViewIs('dashboard.pages.auth.login');
+    }
+
+    /** @test */
+    function admin_can_login()
+    {
+        factory(Admin::class)->create([
+            'email' => 'example@example.com',
+            'password' => bcrypt('secret'),
+        ]);
+
+        $response = $this->post(route('login', ['email' => 'example@example.com', 'password' => 'secret']));
+
+        $response->assertSessionHasNoErrors();
+        $response->assertRedirect(route('dashboard.home'));
+    }
+
+    /** @test */
+    function it_displays_error_on_invalid_email()
+    {
+        factory(Admin::class)->create([
+            'email' => 'example@example.com',
+            'password' => bcrypt('secret'),
+        ]);
+
+        $response = $this->post(route('login', ['email' => 'example🤬@example.com', 'password' => 'secret']));
+
+        $response->assertSessionHasErrors('email');
+    }
+
+    /** @test */
+    function it_displays_error_on_invalid_password()
+    {
+        factory(Admin::class)->create([
+            'email' => 'example@example.com',
+            'password' => bcrypt('secret'),
+        ]);
+
+        $response = $this->post(route('login', ['email' => 'example@example.com', 'password' => '🤬secret']));
+
+        $response->assertSessionHasErrors('email');
+    }
+}
