205654ed4c95f3d21716269462cbe406fe926c95
Alies Lapatsin <web.lapateen@gmail.com>
2019-04-10 20:12:26 +0300

    #271 Fix widget campaign page and remove useless route

--- resources/views/widget/pages/campaigns/index.blade.php
+++
@@ -1,5 +0,0 @@
-@extends('widget.layouts.main')
-
-@section('body')
-    <h1>Not implemented yet, you can do it 👍</h1>
-@endsection
--- resources/views/widget/pages/campaigns/show.blade.php
+++ resources/views/widget/pages/campaigns/show.blade.php
@@ -1,7 +1,17 @@
+<?php
+/**
+ * @var \Diglabby\Doika\Models\Campaign $campaign
+ */
+?>
+
 @extends('widget.layouts.main')
 
-@section('body_class', 'page-home-campaign')
-
 @section('body')
-    <div id="app"></div>
+    <h1>Not implemented yet, you can do it 👍</h1>
+    <p>Name: {{ $campaign->name }}</p>
+    <p>Description: {{ $campaign->description }}</p>
+    <img src="{{ $campaign->picture_url }}" alt="{{ $campaign->name }}">
+    <p>Target amount: {{ $campaign->target_amount }}</p>
+    <p>Currency: {{ $campaign->currency }}</p>
+    <p>Is active: {{ $campaign->active_status }}</p>
 @endsection
