78c5f58443919f3e868aaaab6304db032cf53767
Alies Lapatsin <web.lapateen@gmail.com>
2019-04-17 17:55:50 +0000

    Apply fixes from StyleCI

--- config/bepaid.php
+++ config/bepaid.php
@@ -4,5 +4,5 @@ return [
     'marketId' => env('BEPAID_MARKET_ID'),
     'marketKey' => env('BEPAID_MARKET_KEY'),
     'live' => env('BEPAID_MARKET_LIVE_MODE'),
-    'recurrent_interval' => '30'
+    'recurrent_interval' => '30',
 ];
