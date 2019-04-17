2b6a46d4e105db4b9e71f02c36acc5b5f0fd4d86
Alies Lapatsin <web.lapateen@gmail.com>
2019-04-17 00:05:53 +0000

    Apply fixes from StyleCI

--- src/Models/Campaign.php
+++ src/Models/Campaign.php
@@ -4,7 +4,6 @@ namespace Diglabby\Doika\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
-use Illuminate\Database\Eloquent\Relations\HasManyThrough;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Support\Carbon;
 
