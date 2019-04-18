4aa1e84cc69eed596f98f4bae8bef9b10cf51703
Alies Lapatsin <web.lapateen@gmail.com>
2019-04-18 08:27:25 +0000

    Apply fixes from StyleCI

--- database/migrations/2019_04_18_064030_create_settings_table.php
+++ database/migrations/2019_04_18_064030_create_settings_table.php
@@ -14,41 +14,40 @@ class CreateSettingsTable extends Migration
     /** @var string */
     private $valueColumn;
 
-	public function __construct()
-	{
-		if (version_compare(Application::VERSION, '5.0', '>=')) {
-			$this->tablename = Config::get('settings.table');
-			$this->keyColumn = Config::get('settings.keyColumn');
-			$this->valueColumn = Config::get('settings.valueColumn');
-		} else {
-			$this->tablename = Config::get('anlutro/l4-settings::table');
-			$this->keyColumn = Config::get('anlutro/l4-settings::keyColumn');
-			$this->valueColumn = Config::get('anlutro/l4-settings::valueColumn');
-		}
-	}
+    public function __construct()
+    {
+        if (version_compare(Application::VERSION, '5.0', '>=')) {
+            $this->tablename = Config::get('settings.table');
+            $this->keyColumn = Config::get('settings.keyColumn');
+            $this->valueColumn = Config::get('settings.valueColumn');
+        } else {
+            $this->tablename = Config::get('anlutro/l4-settings::table');
+            $this->keyColumn = Config::get('anlutro/l4-settings::keyColumn');
+            $this->valueColumn = Config::get('anlutro/l4-settings::valueColumn');
+        }
+    }
 
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-		Schema::create($this->tablename, function(Blueprint $table)
-		{
-			$table->bigIncrements('id');
-			$table->string($this->keyColumn)->index();
-			$table->text($this->valueColumn);
-		});
-	}
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create($this->tablename, function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string($this->keyColumn)->index();
+            $table->text($this->valueColumn);
+        });
+    }
 
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-		Schema::dropIfExists($this->tablename);
-	}
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists($this->tablename);
+    }
 }
