b94c3f359afdfa6688d658da7d3f5b882f2f8055
Alies Lapatsin <web.lapateen@gmail.com>
2019-04-18 11:10:13 +0300

    #262 Add tests for SettingsController

---
+++ tests/Feature/Http/Controllers/Dashboard/SettingsControllerTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Tests\Feature\Http\Controllers\Dashboard;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class SettingsControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function it_gets_settings()
+    {
+        setting(['existing_key' => 'value'])->save();
+
+        $response = $this->withoutMiddleware()
+            ->get(route('dashboard.settings.index', ['settings' => ['existing_key', 'not_existing_key']]));
+
+        $response->assertJson(['settings' => [
+            'existing_key' => 'value',
+            'not_existing_key' => null,
+        ]]);
+    }
+
+    /** @test */
+    public function it_stores_settings()
+    {
+        $response = $this->withoutMiddleware()
+            ->post(route('dashboard.settings.store'), ['settings' => [
+                'string_value' => 'string value',
+                'empty_value' => '',
+                'numeric_value' => 2,
+            ]]);
+
+        $response->assertJson(['settings' => [
+            'string_value' => 'string value',
+            'empty_value' => '',
+            'numeric_value' => '2',
+        ]]);
+    }
+}
