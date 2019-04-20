4980890f13ee47bb5557844d8b7697204f45eb89
Alexey Kirpichenkau <alexwish@tut.by>
2019-04-20 03:44:49 +0300

    Fix paths in vue app

--- resources/js/dashboard/components/Header.vue
+++ resources/js/dashboard/components/Header.vue
@@ -1,9 +1,11 @@
 <template>
   <Header fixed>
+
     <SidebarToggler class="d-lg-none" display="md" mobile></SidebarToggler>
+
     <a class="navbar-brand" :href="$app.homePath" target="_blank">
-      <img class="navbar-brand-full" src="../../../sass/vendor/tabler/brand/logo.png" height="30" alt="Doika-logo">
-      <img class="navbar-brand-minimized" src="../../../sass/vendor/tabler/brand/logo-symbol.png" width="30" height="30" alt="Doika-logo">
+      <img class="navbar-brand-full" src="/doika/public/images/dashboard/logo.png" height="30" alt="Doika-logo">
+      <img class="navbar-brand-minimized" src="/doika/public/images/dashboard/logo-symbol.png" width="30" height="30" alt="Doika-logo">
     </a>
     <SidebarToggler class="d-md-down-none" display="lg"></SidebarToggler>
     <b-navbar-nav class="ml-auto">
@@ -37,16 +39,12 @@
       </HeaderDropdown>
       <HeaderDropdown right class="px-3">
         <template slot="header">
-          <img :src="this.$app.user.avatar" class="img-avatar" :alt="$t('labels.admin.accountMenu.avatar')">
           <span class="d-md-down-none">
             {{ this.$app.user.name }}
           </span>
         </template>
         <template slot="dropdown">
-          <!--<b-dropdown-item :href="$app.route('user.account')">
-            <i class="fe fe-user"></i>&nbsp;&nbsp;{{ $t('labels.admin.accountMenu.header') }}
-          </b-dropdown-item>-->
-          <b-dropdown-item :href="$app.route('admin.logout')">
+          <b-dropdown-item :href="$app.route('logout')">
             <i class="fe fe-log-out"></i>&nbsp;&nbsp;{{ $t('labels.admin.accountMenu.logout') }}
           </b-dropdown-item>
         </template>
