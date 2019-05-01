221fc93e152794788a2153b101480e41436e0cd2
Alexey Kirpichenkau <alexwish@tut.by>
2019-05-01 17:16:46 +0300

    Змяніў шляхі рэсурсаў для віджэта

--- resources/views/widget/layouts/main.blade.php
+++ resources/views/widget/layouts/main.blade.php
@@ -6,12 +6,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>Модуль Doika | {{ config('app.name') }}</title>
+    <title>Модуль Doika | {{ config('app.name') }}</title>   
 
-    <link rel="stylesheet" href="{{ asset('build/css/widget.css') }}">
+    <link rel="stylesheet" href={{ asset(mix("build/css/widget/widget.css")) }}>
 
-    <script defer src="{{ asset('build/js/vendor-widget.js') }}"></script>
-    <script defer src="{{ asset('build/js/widget.js') }}"></script>
+    <script defer src={{ asset(mix("build/js/widget/widget.js")) }}></script>
 </head>
 <body class="@yield('body_class')">
     @yield('body')
