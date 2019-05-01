d85a90ca7dd180c08a519991e5b694678a955690
Alies Lapatsin <web.lapateen@gmail.com>
2019-05-02 02:29:03 +0300

    #356 Prefix all routes by `/`

--- routes/widget.php
+++ routes/widget.php
@@ -12,10 +12,10 @@
 |
 */
 
-Route::get('campaigns/{campaign}', 'Widget\CampaignController@show')->name('widget.campaign.show')->where('campaignId', '[0-9]+');
-Route::get('campaigns/{campaign}/donation-result', 'Widget\CampaignDonationResultController@show')->name('widget.campaign.donation-result');
+Route::get('/campaigns/{campaign}', 'Widget\CampaignController@show')->name('widget.campaign.show')->where('campaignId', '[0-9]+');
+Route::get('/campaigns/{campaign}/donation-result', 'Widget\CampaignDonationResultController@show')->name('widget.campaign.donation-result');
 
-Route::get('help', 'Widget\HelpController@show')->name('widget.help.show');
+Route::get('/help', 'Widget\HelpController@show')->name('widget.help.show');
 
-Route::get('feedback', 'Widget\FeedbackController@show')->name('widget.feedbacks.show');
-Route::post('feedback', 'Widget\FeedbackController@store')->name('widget.feedbacks.store');
+Route::get('/feedback', 'Widget\FeedbackController@show')->name('widget.feedbacks.show');
+Route::post('/feedback', 'Widget\FeedbackController@store')->name('widget.feedbacks.store');
