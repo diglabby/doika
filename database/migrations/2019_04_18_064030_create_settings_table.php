10d65fffe319400509195501ff293bbcfa54b427
Alies Lapatsin <web.lapateen@gmail.com>
2019-05-10 21:35:12 +0300

    #425 Simplify CreateSettingsTable migration

--- database/migrations/2019_04_18_064030_create_settings_table.php
+++ database/migrations/2019_04_18_064030_create_settings_table.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Config;
 
 class CreateSettingsTable extends Migration
@@ -16,15 +15,9 @@ class CreateSettingsTable extends Migration
 
     public function __construct()
     {
-        if (version_compare(Application::VERSION, '5.0', '>=')) {
-            $this->tablename = Config::get('settings.table');
-            $this->keyColumn = Config::get('settings.keyColumn');
-            $this->valueColumn = Config::get('settings.valueColumn');
-        } else {
-            $this->tablename = Config::get('anlutro/l4-settings::table');
-            $this->keyColumn = Config::get('anlutro/l4-settings::keyColumn');
-            $this->valueColumn = Config::get('anlutro/l4-settings::valueColumn');
-        }
+        $this->tablename = Config::get('settings.table');
+        $this->keyColumn = Config::get('settings.keyColumn');
+        $this->valueColumn = Config::get('settings.valueColumn');
     }
 
     /**
