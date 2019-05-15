af7bf7c4cee7386fb9fd80dbb83dbe39e30eee06
Alies Lapatsin <web.lapateen@gmail.com>
2019-05-15 20:18:42 +0300

    #433 Fix helper name

--- resources/views/dashboard/layouts/app.blade.php
+++ resources/views/dashboard/layouts/app.blade.php
@@ -66,7 +66,7 @@
                         @else
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                    {{ app()->user()->name }} <span class="caret"></span>
+                                    {{ auth()->user()->name }} <span class="caret"></span>
                                 </a>
 
                                 <ul class="dropdown-menu" role="menu">
