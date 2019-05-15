91b07a661ed0f8bf66af08930eb0543e400d58c1
Alies Lapatsin <web.lapateen@gmail.com>
2019-05-16 00:12:22 +0300

    Update Admin seeder to underline demo mode of demo admin

--- database/factories/AdminFactory.php
+++ database/factories/AdminFactory.php
@@ -15,7 +15,7 @@ $factory->define(Admin::class, function (Faker $faker) {
         'name' => $faker->name,
         'email' => $faker->email,
         'email_verified_at' => now(),
-        'password' => Hash::make('password'), // password
+        'password' => Hash::make('password'),
         'remember_token' => Str::random(10),
     ];
 });
--- database/seeds/AdminSeeder.php
+++ database/seeds/AdminSeeder.php
@@ -3,14 +3,14 @@
 use Diglabby\Doika\Models\Admin;
 use Illuminate\Database\Seeder;
 
-class AdminSeeder extends Seeder
+final class AdminSeeder extends Seeder
 {
     public function run()
     {
         factory(Admin::class)->create([
             'email' => 'demo@example.com',
             'password' => Hash::make('demo'), // demo
-            'name' => 'Admin',
+            'name' => 'Demo Admin',
         ]);
     }
 }
