5fb0cb39583fb75d44bfef72cf97ba3481da042c
Alies Lapatsin <web.lapateen@gmail.com>
2019-05-29 01:02:57 +0300

    #482 Fix Campaign factory to property generate `visual_settings`

--- database/factories/CampaignFactory.php
+++ database/factories/CampaignFactory.php
@@ -17,7 +17,16 @@ $factory->define(Campaign::class, function (Faker\Generator $faker) {
         'active_status' => 1,
         'started_at' => today(),
         'finished_at' => today()->addMonths(1),
-        'visual_settings' => '{"buttons":["5","10","25","50"],"progressBar":"1","colors":{"widgetBackground":null,"buttonBackground":null,"progressBarColor":null,"fontColor":null,"termsOfUse":null}}',
+        'visual_settings' => [
+            'buttons' => [5, 10, 25, 50],
+            'progressBar' => true,
+            'colors' => [
+                'widgetBackground' => null,
+                'buttonBackground' => null,
+                'progressBarColor' => null,
+                'fontColor' => null,
+            ],
+        ],
     ];
 });
 
