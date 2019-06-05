ae445020428813b830c11a65736e6f575199b9ce
Alies Lapatsin <web.lapateen@gmail.com>
2019-06-05 14:28:46 +0300

    #499 Add public testing bePaid data to testing seeder

--- database/seeds/TestingSeeder.php
+++ database/seeds/TestingSeeder.php
@@ -16,5 +16,9 @@ final class TestingSeeder extends Seeder
         factory(Transaction::class, 27)->create();
         factory(Subscription::class, 12)->create(['campaign_id' => $campaigns->first()->id]);
         factory(Subscription::class, 2)->state('canceled')->create(['campaign_id' => $campaigns->last()->id]);
+
+        \DB::table('settings')->insert(['key' => 'gateway_bePaid_idMarket', 'value' => 363]);
+        \DB::table('settings')->insert(['key' => 'gateway_bePaid_keyMarket', 'value' => '4f585d2709776e53d080f36872fd1b63b700733e7624dfcadd057296daa37df6']);
+        \DB::table('settings')->insert(['key' => 'gateway_bePaid_mode', 'value' => 'test']);
     }
 }
