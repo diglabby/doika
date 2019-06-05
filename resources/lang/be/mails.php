d36dc6a1cedd129dbe88a2cd81b7d05b4cb2695b
Alies Lapatsin <web.lapateen@gmail.com>
2019-06-06 02:50:31 +0300

    #464 Add lang files for emails

---
+++ resources/lang/be/mails.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'layout' => [
+        'hello' => 'Вітаем!',
+        'regards' => 'З павагай',
+    ],
+];
---
+++ resources/lang/en/mails.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'layout' => [
+        'hello' => 'Hello!',
+        'regards' => 'Best regards',
+    ],
+];
