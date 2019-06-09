2de8659e35156e4816bc67222bbbd0df8889ad02
Alies Lapatsin <web.lapateen@gmail.com>
2019-06-10 02:58:40 +0300

    #149 Add simple phone number validation (backend)

--- app/Http/Controllers/Widget/CampaignSubscriptionIntendController.php
+++ app/Http/Controllers/Widget/CampaignSubscriptionIntendController.php
@@ -23,6 +23,7 @@ final class CampaignSubscriptionIntendController extends Controller
             'currency_code' => ['string', 'size:3'],
             'payment_interval' => ['required', new IsoTimeDuration()], // P1M (means 1 month)
             'email' => ['required', 'email'],
+            'phone' => ['nullable', 'string', 'max:15'],
         ]);
 
         if ($campaign->isFinished()) {
@@ -36,6 +37,8 @@ final class CampaignSubscriptionIntendController extends Controller
             'email' => $request->get('email'),
             'name' => $request->get('name'),
         ]);
+
+        $donator->update(['phone' => $request->get('phone')]);
 
         $money = new Money($request->get('amount'), new Currency($request->get('currency_code')));
         $interval = new CarbonInterval($request->get('payment_interval'));
