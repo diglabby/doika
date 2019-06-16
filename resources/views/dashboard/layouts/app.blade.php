9e56f9de2850519641d381e9edd4673f4ceadffb
Cema93 <2507496@gmail.com>
2019-06-16 21:32:06 +0300

    Исправил фавикон

--- resources/views/dashboard/layouts/app.blade.php
+++ resources/views/dashboard/layouts/app.blade.php
@@ -1,7 +1,8 @@
 <!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}">
 <head>
-    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+    <link rel="icon" href="{{ asset('images/Doika-logo.png') }}" type="image/x-icon" />
+    <link rel="shortcut icon" href="{{ asset('images/Doika-logo.png') }}" type="image/x-icon" />
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
