61296cefbc18425f65b2c5db15d4d8f1c8cf83a0
Ryzomych <rizoma@mail.riri>
2020-03-30 11:47:07 +0300

    cleaned switcher component + added notification about success saving of setting and removed all weird moments relating checked attribute and added a pending (emptry) method

--- resources/js/dashboard/components/Plugins/Switch.vue
+++ resources/js/dashboard/components/Plugins/Switch.vue
@@ -3,8 +3,6 @@
     <input type="checkbox"
            :name="name"
            class="custom-switch-input"
-           :value="value"
-           :checked="isChecked"
            @change="handleChange"
     >
     <span class="custom-switch-indicator"></span>
@@ -15,14 +13,11 @@
 <script>
 export default {
   model: {
-    prop: 'checked',
+
     event: 'change'
   },
   props: {
-    value: {
-      type: Boolean,
-      default: true
-    },
+
     uncheckedValue: {
       type: Boolean,
       default: false
@@ -41,13 +36,10 @@ export default {
     }
   },
   computed: {
-    isChecked() {
-      return this.checked === this.value
-    }
   },
   methods: {
     handleChange({ target: { checked } }) {
-      this.$emit('change', checked ? this.value : this.uncheckedValue)
+
     }
   }
 }
