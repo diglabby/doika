fc3f277caa7d5cd97116f3db9651bf39d000d9fe
Alexey <darth.tesla47@gmail.com>
2020-05-26 13:45:06 +0300

    add focus on input by click on picker button

--- resources/js/dashboard/components/Plugins/ColorPicker.vue
+++ resources/js/dashboard/components/Plugins/ColorPicker.vue
@@ -1,7 +1,7 @@
 <template>
     <b-input-group class="mt-3 colorpicker">
         <p class="colorpicker__desc">{{ section_name }}</p>
-        <b-form-input class="colorpicker__input" v-model="colorValue" @click="togglePicker" @input="updateFromInput"></b-form-input>
+        <b-form-input ref="input" class="colorpicker__input" v-model="colorValue" @input="updateFromInput"></b-form-input>
         <b-input-group-append>
             <b-button class="color-picker__button" variant="primary"><span class="current-color" :style="'background-color: ' + colorValue" @click="togglePicker"></span></b-button>
                 <chrome-picker :value="colors" @input="updateFromPicker" v-if="displayPicker"></chrome-picker>
@@ -55,6 +55,7 @@ export default {
             }
         },
         showPicker() {
+            this.$refs.input.focus()
             setTimeout(() => {
                 document.addEventListener('click', this.documentClick);
             }, 0)
@@ -80,7 +81,7 @@ export default {
             }
         },
         documentClick(e) {
-            if(this.displayPicker && !e.target.closest(".vc-chrome")) {
+            if(this.displayPicker && !e.target.closest(".vc-chrome") && !e.target.classList.contains("colorpicker__input")) {
                 this.hidePicker()
         }}
     },
