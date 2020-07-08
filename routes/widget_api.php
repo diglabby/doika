fb4ee5e789996ec30ffe8b364d79bc61e8858531
Viktor Poyarkov <pojarkov@tut.by>
2020-07-08 10:38:40 +0300

    Hot fix in routes for widget api

--- routes/widget_api.php
+++ routes/widget_api.php
@@ -13,4 +13,4 @@ Route::get('/help', 'Widget\HelpController@show')->name('widget.help.show');
 
 Route::get('/feedback', 'Widget\FeedbackController@show')->name('widget.feedbacks.show');
 Route::post('/feedback', 'Widget\FeedbackController@store')->name('widget.feedbacks.store');
-Route::post('/check-payment-status', 'Widget\BePaidCheckPaymentStatus@checkPaymentStatus')->name('widget.bepaid.check-payment-status');
+Route::post('/bepaid-check-payment-status', 'Widget\BePaidCheckPaymentStatusController@checkPaymentStatus')->name('widget.bepaid.check-payment-status');
