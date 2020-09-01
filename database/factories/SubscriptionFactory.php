fc309ae34ae28f42ca3e52a4d537a4472c726e9f
Alies Lapatsin <lptn@users.noreply.github.com>
2020-09-01 10:56:12 +0000

    Apply fixes from StyleCI

--- database/factories/SubscriptionFactory.php
+++ database/factories/SubscriptionFactory.php
@@ -11,20 +11,20 @@ use Diglabby\Doika\Models\Subscription;
  */
 $factory->define(Subscription::class, function (\Faker\Generator $faker) {
     return [
-        'donator_id' => factory(Donator::class)->lazy(),
-        'campaign_id' => factory(Campaign::class)->lazy(),
-        'payment_gateway' => 'bePaid',
+        'donator_id'                      => factory(Donator::class)->lazy(),
+        'campaign_id'                     => factory(Campaign::class)->lazy(),
+        'payment_gateway'                 => 'bePaid',
         'payment_gateway_subscription_id' => $faker->uuid,
-        'amount' => $faker->numberBetween(1, 100) * 100, // in cents
-        'currency' => 'BYN',
-        'payment_interval' => 'P1M',
-        'cancel_reason' => null,
+        'amount'                          => $faker->numberBetween(1, 100) * 100, // in cents
+        'currency'                        => 'BYN',
+        'payment_interval'                => 'P1M',
+        'cancel_reason'                   => null,
     ];
 });
 
 $factory->state(Subscription::class, 'canceled', function () {
     return [
         'cancel_reason' => "Customer's request",
-        'deleted_at' => now()->subMinutes(10),
+        'deleted_at'    => now()->subMinutes(10),
     ];
 });
