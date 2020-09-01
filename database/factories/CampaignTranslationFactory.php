6aa294398b7562e859d08fb3499b630f3e7197fb
lptn <lptn@users.noreply.github.com>
2020-09-01 10:48:02 +0000

    ✨ Apply coding style fixes to PHP

--- database/factories/CampaignTranslationFactory.php
+++ database/factories/CampaignTranslationFactory.php
@@ -10,9 +10,9 @@ use Diglabby\Doika\Models\CampaignTranslation;
  */
 $factory->define(CampaignTranslation::class, function (Faker\Generator $faker) {
     return [
-        'locale'       => $faker->randomElement(['be', 'ru', 'en']),
-        'name'         => $faker->words(3, true),
-        'description'  => $faker->imageUrl(),
+        'locale' => $faker->randomElement(['be', 'ru', 'en']),
+        'name' => $faker->words(3, true),
+        'description' => $faker->imageUrl(),
         'terms_of_use' => "<div>{$faker->sentence}<div>",
     ];
 });
