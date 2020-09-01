9ea0eae9d80319781c920df9ecdcab01a52b4702
Alies Lapatsin <lptn@users.noreply.github.com>
2020-09-01 10:48:45 +0000

    Apply fixes from StyleCI

--- database/seeds/AdminSeeder.php
+++ database/seeds/AdminSeeder.php
@@ -8,9 +8,9 @@ final class AdminSeeder extends Seeder
     public function run()
     {
         factory(Admin::class)->create([
-            'email' => 'demo@example.com',
+            'email'    => 'demo@example.com',
             'password' => Hash::make('demo'), // demo
-            'name' => 'Demo Admin',
+            'name'     => 'Demo Admin',
         ]);
     }
 }
