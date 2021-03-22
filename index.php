c7465e127d864a0074eda9a3bdb8a4ffc9257925
Alies Lapatsin <web.lapateen@gmail.com>
2021-03-23 00:20:49 +0300

    #788 Дадае хак каб дойка працавала у абодвух асяроддзях: лакальна і ў докеры

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+|--------------------------------------------------------------------------
+| Fallback file
+|--------------------------------------------------------------------------
+|
+| This file is just a fallback for old doika installations,
+| where we copy the whole Doika app to the web-server dir (e.g. /etc/www/html).
+| For new Doika installations, doika should be located outside a directory server by web-server server,
+| but doika/public directory should be linked to as 'doika' to web-server directory.
+|
+*/
+require_once __DIR__.'/public/index.php';
