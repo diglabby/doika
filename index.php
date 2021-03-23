fef11ca9dec9803f065abc971cbdb729b9c4d2e3
Alies Lapatsin <web.lapateen@gmail.com>
2021-03-23 14:31:42 +0300

    #792 Паказвае паведамленне аб памылцы пры няправільнай наладзе

--- index.php
+++ index.php
@@ -13,4 +13,10 @@ declare(strict_types=1);
 | but doika/public directory should be linked to as 'doika' to web-server directory.
 |
 */
+
+if (getenv('IS_DOIKA_SYMLINKED') === '1') {
+    echo 'Web server should not have an access to this index.php when IS_DOIKA_SYMLINKED is used';
+    exit(1);
+}
+
 require_once __DIR__.'/public/index.php';
