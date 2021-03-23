420d4a370632bf0004a62aef60a71fdbae1d38bf
Alies Lapatsin <web.lapateen@gmail.com>
2021-03-23 10:15:30 +0300

    #787 Дадае тэсты для doika_route

--- routes/dashboard_api.php
+++ routes/dashboard_api.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * URIs prefixed by /doika/dashboard/api.
+ * URIs prefixed by /{subdir}/dashboard/api.
  *
  * @see \Diglabby\Doika\Providers\RouteServiceProvider::mapDashboardRoutes()
  * Applied middleware groups: api, auth
---
+++ tests/Unit/Helpers/DoikaRouteTest.php
@@ -0,0 +1,40 @@
+<?php declare(strict_types=1);
+
+namespace Tests\Unit\Helpers;
+
+use Illuminate\Support\Facades\Config;
+use Tests\TestCase;
+
+/** @internal */
+final class DoikaRouteTest extends TestCase
+{
+    /** @test */
+    public function it_generates_a_route_for_doika_installed_to_site_root(): void
+    {
+        Config::set('doika.url_prefix', 'doika');
+
+        $doikaRoute = doika_route('dashboard.login', [], false);
+
+        $this->assertSame('/dashboard/login', $doikaRoute);
+    }
+
+    /** @test */
+    public function it_generates_a_route_for_doika_installed_to_subdirectory(): void
+    {
+        Config::set('doika.url_prefix', 'doika');
+
+        $doikaRoute = doika_route('dashboard.login', [], false);
+
+        $this->assertSame('/doika/dashboard/login', $doikaRoute);
+    }
+
+    /** @test */
+    public function it_generates_a_route_for_doika_installed_to_subdirectory_2nd_lvl(): void
+    {
+        Config::set('doika.url_prefix', 'payments/doika');
+
+        $doikaRoute = doika_route('dashboard.login', [], false);
+
+        $this->assertSame('/payments/doika/dashboard/login', $doikaRoute);
+    }
+}
