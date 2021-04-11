9a7b2903b2145009af658c511481d3d720f5160b
Alies Lapatsin <web.lapateen@gmail.com>
2021-04-11 21:35:28 +0300

    #732 Паляпшае чытабельнасць коду

--- resources/js/widget/views/DonateWindow.vue
+++ resources/js/widget/views/DonateWindow.vue
@@ -94,12 +94,9 @@ export default {
     };
   },
 
-
-
   methods: {
     delay
   },
-
 
   async created() {
 
@@ -128,13 +125,7 @@ export default {
 
   setTimeout(function() { delay(1, curToken, ''); }, firstLaunch);
 
-
-  var options = {
-      type: 'inline',
-      id: 'paymentForm',
-      url: this.redirect_url,
-      style:
-        'html {\
+  const cssStylesForBePaidIframe = 'html {\
     padding:0;\
     margin:0;\
     overflow: hidden;\
@@ -282,167 +273,173 @@ export default {
               height: 186px;\
               position:relative;\
               background: url("https://' +
-        parent.document.location.host +
-        '/doika/images/front-card.png"),\
-    url("https://' +
-        parent.document.location.host +
-        '/doika/images/back-card.png");\
-    background-repeat: no-repeat;\
-    background-position: left top, right 20px;\
+    parent.document.location.host +
+    '/doika/images/front-card.png"),\
+url("https://' +
+    parent.document.location.host +
+    '/doika/images/back-card.png");\
+background-repeat: no-repeat;\
+background-position: left top, right 20px;\
+}\
+#request .verification-value label {\
+display: none;\
+}\
+#request_customer_email {\
+      width: 250px !important;\
+      margin: auto !important;\
+      margin-top: 20px !important;\
+      pointer-events: auto !important;\
+      font-family: Ubuntu, sans-serif !important;\
+      font-size: 18px !important;\
+      font-weight: 500 !important;\
+      text-align: left !important;\
+      width: 250px !important;\
+      height: 50px !important;\
+      color: #191919 !important;\
+      border: none !important;\
+      -webkit-box-sizing: border-box !important;\
+      box-sizing: border-box !important;\
+      padding: 0px 5px 0px 15px !important;\
+      color: #31383e !important;\
+      -moz-appearance: textfield !important;\
+      -webkit-appearance: none !important;\
+      -webkit-box-shadow: 0 3px 6px rgba(0, 0, 0, 0.07), 0 3px 6px rgba(0, 0, 0, 0.08) !important;\
+      box-shadow: 0 3px 6px rgba(0, 0, 0, 0.07), 0 3px 6px rgba(0, 0, 0, 0.08) !important;\
+      -webkit-transition: all 0.3s ease 0s !important;\
+      transition: all 0.3s ease 0s !important;\
   }\
-  #request .verification-value label {\
-    display: none;\
-  }\
-  #request_customer_email {\
-          width: 250px !important;\
-          margin: auto !important;\
-          margin-top: 20px !important;\
-          pointer-events: auto !important;\
-          font-family: Ubuntu, sans-serif !important;\
-          font-size: 18px !important;\
-          font-weight: 500 !important;\
-          text-align: left !important;\
-          width: 250px !important;\
-          height: 50px !important;\
-          color: #191919 !important;\
-          border: none !important;\
-          -webkit-box-sizing: border-box !important;\
-          box-sizing: border-box !important;\
-          padding: 0px 5px 0px 15px !important;\
-          color: #31383e !important;\
-          -moz-appearance: textfield !important;\
-          -webkit-appearance: none !important;\
-          -webkit-box-shadow: 0 3px 6px rgba(0, 0, 0, 0.07), 0 3px 6px rgba(0, 0, 0, 0.08) !important;\
-          box-shadow: 0 3px 6px rgba(0, 0, 0, 0.07), 0 3px 6px rgba(0, 0, 0, 0.08) !important;\
-          -webkit-transition: all 0.3s ease 0s !important;\
-          transition: all 0.3s ease 0s !important;\
-      }\
-  .email-icon {\
-    display: none;\
-  }\
-  .credentials {\
-    white-space: nowrap;\
-  }\
-  #beGatewayForm #iframe {\
-    width: 100%;\
-  }\
-  #iframe .content {\
-  	margin: 0 !important;\
-  }\
-  #request {\
-  	margin:0;\
-  }\
-  #request .submit-button .pure-button input {\
-      padding: 10px !important;\
-      height: 50px;\
-      width: 165px;\
-      overflow: hidden;\
-      background-color: #FF80B4;\
-      margin-bottom: 10px;\
-  }\
-  #request > div.submit-button > div.pure-button.pure-button-success.no-padding > input[type=submit] {\
-  background-color:  #94c840\
-  margin-top: 0;\
-  padding:10px !important;\
+.email-icon {\
+display: none;\
+}\
+.credentials {\
+white-space: nowrap;\
+}\
+#beGatewayForm #iframe {\
+width: 100%;\
+}\
+#iframe .content {\
+margin: 0 !important;\
+}\
+#request {\
+margin:0;\
+}\
+#request .submit-button .pure-button input {\
+  padding: 10px !important;\
+  height: 50px;\
+  width: 165px;\
+  overflow: hidden;\
   background-color: #FF80B4;\
-  color: #303030;\
-  height: 100%;\
+  margin-bottom: 10px;\
+}\
+#request > div.submit-button > div.pure-button.pure-button-success.no-padding > input[type=submit] {\
+background-color:  #94c840\
+margin-top: 0;\
+padding:10px !important;\
+background-color: #FF80B4;\
+color: #303030;\
+height: 100%;\
+}\
+#request .submit-button .pure-button input, .submit-button .pure-button {\
+background-color:  #94c840;\
+border-radius: 2px;\
+padding: 0;\
+height: 32px;\
+color: rgba(231, 238, 244, 0.91);\
+width: 100%;\
+height: 30px;\
+font-weight: 700;\
+font-size: 17px;\
+margin-top: 10.5px;\
+\
+}\
+body#iframe .credentials p {\
+  vertical-align: bottom;\
+}\
+body#iframe .credentials img {\
+vertical-align: baseline;\
+}\
+#request .exp-date input {\
+width: 90px;\
+}\
+.content .credit_card .credit-card {\
+margin: auto;\
+}\
+#request .submit-button {\
+  margin: 0px auto;\
+  display: flex;\
+  flex-direction: column;\
   }\
-  #request .submit-button .pure-button input, .submit-button .pure-button {\
-  	background-color:  #94c840;\
-  	border-radius: 2px;\
-  	padding: 0;\
-  	height: 32px;\
-  	color: rgba(231, 238, 244, 0.91);\
-    width: 100%;\
-    height: 30px;\
-  	font-weight: 700;\
-  	font-size: 17px;\
-  	margin-top: 10.5px;\
-  	\
-  }\
-  body#iframe .credentials p {\
-      vertical-align: bottom;\
-  }\
-  body#iframe .credentials img {\
-    vertical-align: baseline;\
-  }\
-  #request .exp-date input {\
-  	width: 90px;\
-  }\
-  .content .credit_card .credit-card {\
-    margin: auto;\
-  }\
-  #request .submit-button {\
-      margin: 0px auto;\
-      display: flex;\
-      flex-direction: column;\
-      }\
-  .submit-button {\
-      padding: 0 !important;\
-  }\
-  .submit-button .pure-button {\
-    padding: 0 !important;\
-    border-radius: 6px;\
-    box-shadow: 0 1px 5px 0px #000;\
-  }\
-  .credit-card {\
-    padding: 0 0 20px!important;\
-    border-radius: 6px;\
-  }\
-  .credentials {\
-  	margin-top: 5px!important;\
-  	font-size: 12px!important;\
-  }\
-  #logos {\
-  	margin-top: -10px!important;\
-  }\
-  .pure-form input[type="text"], .pure-form input[type="tel"] {\
-  	padding: 0;\
-  }\
-  .submit-button .in-progress {\
-    margin: 4px 0;\
-    display:none !important;\
-  }\
-  @media screen and (max-width: 321px) {\
-    .credentials {\
-      font-size: 10px !important;\
-    }\
-  }\
-  @media screen and (max-width: 250px) {\
-    .inline-logo img {\
-      max-height: 40px;\
-    }\
-    #logos-container {\
-        margin-top: 10px;\
-    }\
-    .credit-card {\
-      width: 100% !important;\
-    }\
-    .request_credit_card_holder input {\
-      width: 60% !important;\
-      left: 20px;\
-    }\
-    #request .verification-value input {\
-      top: 130px;\
-    }\
-    .request_credit_card_exp_date {\
-      left: 135px;\
-      width: 80px;\
-    }\
-    .request_credit_card_number input {\
-      top: 30px;\
-      position: absolute;\
-      left: 25px;\
-      width: 80% !important;\
-    }\
-  }\
-  ',
+.submit-button {\
+  padding: 0 !important;\
+}\
+.submit-button .pure-button {\
+padding: 0 !important;\
+border-radius: 6px;\
+box-shadow: 0 1px 5px 0px #000;\
+}\
+.credit-card {\
+padding: 0 0 20px!important;\
+border-radius: 6px;\
+}\
+.credentials {\
+margin-top: 5px!important;\
+font-size: 12px!important;\
+}\
+#logos {\
+margin-top: -10px!important;\
+}\
+.pure-form input[type="text"], .pure-form input[type="tel"] {\
+padding: 0;\
+}\
+.submit-button .in-progress {\
+margin: 4px 0;\
+display:none !important;\
+}\
+@media screen and (max-width: 321px) {\
+.credentials {\
+  font-size: 10px !important;\
+}\
+}\
+@media screen and (max-width: 250px) {\
+.inline-logo img {\
+  max-height: 40px;\
+}\
+#logos-container {\
+    margin-top: 10px;\
+}\
+.credit-card {\
+  width: 100% !important;\
+}\
+.request_credit_card_holder input {\
+  width: 60% !important;\
+  left: 20px;\
+}\
+#request .verification-value input {\
+  top: 130px;\
+}\
+.request_credit_card_exp_date {\
+  left: 135px;\
+  width: 80px;\
+}\
+.request_credit_card_number input {\
+  top: 30px;\
+  position: absolute;\
+  left: 25px;\
+  width: 80% !important;\
+}\
+}\
+';
+
+  const bePaidOptions = {
+      type: 'inline',
+      id: 'paymentForm',
+      url: this.redirect_url,
+      style: cssStylesForBePaidIframe,
       size: { width: 450, height: 450 }
     };
 
-    var pf = new BeGateway(options);
-    pf.buildForm();
+    /** @see https://github.com/begateway/begateway-js/blob/master/Readme.md **/
+    var pf = new BeGateway(bePaidOptions);
   }
 };
 </script>
